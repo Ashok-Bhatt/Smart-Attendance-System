8ce9058ef8077bd89dcf9fd980ef4805c1a115b9
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-09-17 22:56:31 +0530

    Resolve bug related to folder path in huggingface_download.py

--- server/huggingface_download.py
+++ server/huggingface_download.py
@@ -4,10 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MODEL_PATH = "./models/student_recognition.keras"
+MODEL_DIR = "./models"
+MODEL_PATH = os.path.join(MODEL_DIR, "student_recognition.keras")
+
 HF_URL = "https://huggingface.co/AshokBhatt/Smart-Attendance/resolve/main/student_recognition.keras"
 headers = headers = {"Authorization": f"Bearer {os.getenv('HF_TOKEN')}"}
 
+# Create directory if not exists
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+# Creating keras file if not present
 if not os.path.exists(MODEL_PATH):
     print("Downloading model from Hugging Face ...")
     r = requests.get(HF_URL, headers=headers)
